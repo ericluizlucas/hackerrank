c39f5aad1c39cbd61af3dd50f5f2566ebaad6c76
eric <eric@2upsolucoes.com.br>
2023-06-24 13:39:54 -0300

    first commit

---
+++ mini-max-sum/Program.cs
@@ -0,0 +1,35 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Int64> arr)
+    {
+        arr.Sort();
+        Int64 maxSum = 0;
+        Int64 minSum = 0;
+
+        for (int i = 0; i < arr.Count(); i++)
+        {
+            if (i != 0) maxSum += arr[i];
+            if (i != (arr.Count() - 1)) minSum += arr[i];
+        }
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<Int64> arr = new List<Int64> { 1, 2, 3, 4, 5 };
+
+        Result.miniMaxSum(arr);
+    }
+}
---
+++ plus-minus/Program.cs
@@ -0,0 +1,40 @@
+﻿class Result
+{
+    public static void plusMinus(List<int> arr)
+    {
+        decimal elementsCount = arr.Count();
+        decimal positiveCount = 0.000000m;
+        decimal negativeCount = 0.000000m;
+        decimal zeroCount = 0.000000m;
+        foreach (var item in arr)
+        {
+            if (item > 0)
+            {
+                positiveCount += 1m;
+            }
+            else if (item < 0)
+            {
+                negativeCount += 1m;
+            }
+            else
+                zeroCount += 1m;
+        }
+
+        Console.WriteLine(Math.Round(positiveCount / elementsCount, 6));
+        Console.WriteLine(Math.Round(negativeCount / elementsCount, 6));
+        Console.WriteLine(Math.Round(zeroCount / elementsCount, 6));
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        // int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int> { -4, 3, -9, 0, 4, 1 };
+        // Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
