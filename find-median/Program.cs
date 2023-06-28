599a23761277426c5b0a0735e86ad1dcb18cb147
eric <eric@2upsolucoes.com.br>
2023-06-28 11:42:24 -0300

    feature find median algorithm

---
+++ find-median/Program.cs
@@ -0,0 +1,29 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<int> arr)
+    {
+        arr.Sort();
+        int indexMiddle = arr.Count()/2;
+        return arr[indexMiddle];
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> arr = new List<int> { 0, 1, 2, 4, 6, 5, 3 };
+
+        int result = Result.findMedian(arr);
+        Console.WriteLine(result);
+    }
+}
