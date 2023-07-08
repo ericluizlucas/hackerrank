d9c7f5b1bea279b424709e95506fe76d4b089ee5
eric <eric@2upsolucoes.com.br>
2023-07-08 00:01:11 -0300

    removing unnecessary comments

--- caesar-cipher/Program.cs
+++ caesar-cipher/Program.cs
@@ -12,38 +12,6 @@
 
     public static string caesarCipher(string s, int k)
     {
-        // List<string> alphabet = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-        // char[] splited = s.ToCharArray();
-        // string[] letters = new string[splited.Count()];
-
-        // for(int j = 0; j < splited.Count(); j++)
-        // {
-        //     bool done = false;
-        //     if(!done)
-        //     {
-        //     for(int i = 0; i<alphabet.Count(); i++)
-        //     {
-        //         var letter = splited[j].ToString();
-        //         bool isUpper = char.IsUpper(splited[j]);
-        //         var nowAlph = isUpper ? alphabet[i].ToUpper() : alphabet[i];
-        //         if(!done) letters[j] = letter;
-
-        //         if(letter == nowAlph)
-        //         {
-        //             letters[j] = (i + k) >= alphabet.Count() ? alphabet[i+k - alphabet.Count()] : alphabet[i+k];
-        //             if(isUpper) letters[j] = letters[j].ToUpper();
-        //             done = true;
-        //         }
-        //     }
-        //     }
-        // }
-        // string resp = "";
-        // foreach(var obj in letters)
-        // {
-        //     resp += obj;
-        // }
-
-        // return resp;
         List<string> alphabet = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         char[] splited = s.ToCharArray();
         string resp = "";
