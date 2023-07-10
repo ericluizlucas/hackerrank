2e6b105e907fb500059de523253da0d667510c54
eric <eric@2upsolucoes.com.br>
2023-07-10 20:21:12 -0300

    feature maximum draws algorithm

---
+++ maximum-draws/Program.cs
@@ -0,0 +1,30 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'maximumDraws' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int maximumDraws(int n)
+    {
+        return n+1;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> numberList = new List<int>() { 1, 2 };
+
+        foreach (var item in numberList)
+        {
+            int result = Result.maximumDraws(item);
+            Console.WriteLine(result);
+        }
+    }
+}
